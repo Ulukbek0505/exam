eea0be7c0e44a351e3e43f43caf1ccf405030050
Dasman99 <kadyrovya.kadyrov@gmail.com>
2022-11-13 14:05:40 +0600

    самые свежие изменения

--- parser.py
+++ parser.py
@@ -11,18 +11,20 @@ def get_response(url):
 
 
 def pizza_mafia(html):
+    data = {}
     soup = BS(html, "html.parser")
     content = soup.find('div', class_= "holiday-decoration menu-bg")
-    title_price = content.find_all("div", class_= "bottom-block_1wBkV")
+    title_price = content.find_all("a", class_= "card_1C7cu")
     for info in title_price:
         title = info.find("div", class_="title_3uNTc").text.strip()
         price = info.find("div", class_="price_1_Tty").text.strip()
         if title == 'Петровская 25 см (толстое с сыром)':
             break
-
-        print(f'{title} - {price}')
+        data.update({title: price})
+    return data
 
 def pizza30cm(html):
+    data ={}
     soup = BS(html, "html.parser")
     content = soup.find('div', class_= "my-catalog uk-section-default uk-section")
     in_title_price = content.find_all("div", {"class": "uk-card uk-card-default uk-card-small"})
@@ -31,10 +33,11 @@ def pizza30cm(html):
         price = info.find("span", class_="woocommerce-Price-amount amount").text.strip()
         if title =='Ранч':
             break
-        print(f'{title} - {price}')
+        data.update({title: price})
+    return data
 
 
 my_html = get_response('https://3332222.ru/menu/picca/')
-my_html1 = get_response('https://pizza30cm.ru/?utm_source=yandex&utm_campaign=%5Bpicca-spb%5D--MK_Con&utm_medium=cpc&utm_term=---autotargeting&utm_content=creative1&_openstat=ZGlyZWN0LnlhbmRleC5ydTs3NDE1ODY3MjsxMjExMDIzOTc2MDt5YW5kZXgucnU6cHJlbWl1bQ&yclid=842516730018529279')
 print(pizza_mafia(my_html))
+my_html1 = get_response('https://pizza30cm.ru/?utm_source=yandex&utm_campaign=%5Bpicca-spb%5D--MK_Con&utm_medium=cpc&utm_term=---autotargeting&utm_content=creative1&_openstat=ZGlyZWN0LnlhbmRleC5ydTs3NDE1ODY3MjsxMjExMDIzOTc2MDt5YW5kZXgucnU6cHJlbWl1bQ&yclid=842516730018529279')
 print(pizza30cm(my_html1))

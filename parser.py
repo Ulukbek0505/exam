25cec73c3d3ed3fca452eecf6e01476d07efb62a
Dasman99 <kadyrovya.kadyrov@gmail.com>
2022-11-14 20:25:51 +0600

    почти всё

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
+
 def get_response(url):
     response = requests.get(url)  # delete # head # post # put...
     if response.status_code == 200:
@@ -11,10 +12,10 @@ def get_response(url):
 
 
 def pizza_mafia(html):
-    data = {}
+    data ={}
     soup = BS(html, "html.parser")
-    content = soup.find('div', class_= "holiday-decoration menu-bg")
-    title_price = content.find_all("a", class_= "card_1C7cu")
+    content = soup.find('div', class_="holiday-decoration menu-bg")
+    title_price = content.find_all("a", class_="card_1C7cu")
     for info in title_price:
         title = info.find("div", class_="title_3uNTc").text.strip()
         price = info.find("div", class_="price_1_Tty").text.strip()
@@ -24,20 +25,30 @@ def pizza_mafia(html):
     return data
 
 def pizza30cm(html):
-    data ={}
+    global menu2
+    data = {}
     soup = BS(html, "html.parser")
-    content = soup.find('div', class_= "my-catalog uk-section-default uk-section")
+    content = soup.find('div', class_="my-catalog uk-section-default uk-section")
     in_title_price = content.find_all("div", {"class": "uk-card uk-card-default uk-card-small"})
     for info in in_title_price:
         title = info.find("h2", class_="uk-h4 uk-margin-small-bottom woocommerce-loop-product__title").text.strip()
         price = info.find("span", class_="woocommerce-Price-amount amount").text.strip()
-        if title =='Ранч':
+        if title == 'Ранч':
             break
-        data.update({title: price})
+
+        # for title, price in data.items():
+        #     data=({title, ":", price})
+        data.update({title:price})
+
     return data
 
 
+
 my_html = get_response('https://3332222.ru/menu/picca/')
+# print(pizza_mafia(my_html))
 print(pizza_mafia(my_html))
 my_html1 = get_response('https://pizza30cm.ru/?utm_source=yandex&utm_campaign=%5Bpicca-spb%5D--MK_Con&utm_medium=cpc&utm_term=---autotargeting&utm_content=creative1&_openstat=ZGlyZWN0LnlhbmRleC5ydTs3NDE1ODY3MjsxMjExMDIzOTc2MDt5YW5kZXgucnU6cHJlbWl1bQ&yclid=842516730018529279')
 print(pizza30cm(my_html1))
+
+# for i in pizza_mafia(my_html):
+#     print(i)
